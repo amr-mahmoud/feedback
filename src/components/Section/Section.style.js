ee4c42efe3b22cec27693ecae3e1023908b7a4c4
amr-mahmoud <amr.hazem.mahmoud@gmail.com>
2021-08-31 23:00:43 +0200

    adding message for empty lists , fixing linting , adding more style , and adding selected logic

--- src/components/Section/Section.style.js
+++ src/components/Section/Section.style.js
@@ -1,44 +1,64 @@
 import styled from "styled-components";
 
-
-
 export const SectionWrapper = styled.div`
 flex:1;
 display:flex;
 flex-direction:column;s
 
-`
+`;
 
-export const SectionHeader=styled.header`
-display:flex;
-flex-direction:row;
-padding: 0 20px;
-justify-content:space-between;
-`
+export const SectionHeader = styled.header`
+  display: flex;
+  flex-direction: row;
+  padding: 0 20px;
+  justify-content: space-between;
+`;
 
 export const AddButtton = styled.button`
-height: 25px;
-margin: auto 0;
-`
-export const StyledInput=styled.input`
+  height: 25px;
+  margin: auto 0;
+`;
+export const StyledInput = styled.input`
+  font-size: 13px;
+  height: 30px;
+  width: 99%;
+  border-radius: 0;
+  border: none;
+  border-bottom: 1px solid black;
+  padding: 0 4px;
+`;
 
-height: 30px;
-width: 98%;
-border-radius: 0;
+export const SectionContent = styled.div`
+  flex: 1;
+  border: 1px solid black;
+  flex-direction: column;
+`;
 
-`
+export const SectionTitle = styled.h3`
+  margin: 10px 0;
+`;
 
-export const SectionContent= styled.div`
-flex:1;
-border:1px solid black;
-flex-direction:column;
-`
+export const StyledListItem = styled.li`
+  font-size: 13px;
+  display: flex;
+  height: 30px;
+  border-bottom: 1px solid black;
+  label {
+    margin: auto 5px;
+  }
+  cursor: pointer;
+  &:hover {
+    background: yellow;
+  }
+`;
 
+export const StyledUl = styled.ul`
+  list-style-type: none;
+  cursor: pointer;
+  padding: 0;
+  margin: 0;
+`;
 
-export const SectionTitle= styled.h3`
-margin: 10px 0
-`
-
-export const StyledListItem=styled.li``
-
-export const StyledUl=styled.ul``
+export const NotifyLabel = styled.label`
+  margin: auto;
+`;
--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -1,53 +1,103 @@
-
-import React,{FC,useEffect,useContext,useState} from "react";
-import {SectionWrapper,SectionTitle,SectionContent,SectionHeader,AddButtton,StyledInput} from './Section.style'
-import {AppContext} from '../../providers'
-import {actionType} from '../../reducer/actions'
+import React, { FC, useEffect, useContext, useState } from "react";
+import {
+  SectionWrapper,
+  SectionTitle,
+  SectionContent,
+  NotifyLabel,
+  SectionHeader,
+  AddButtton,
+  StyledInput,
+  StyledListItem,
+  StyledUl,
+} from "./Section.style";
+import { AppContext } from "../../providers";
+import { actionType } from "../../reducer/actions";
 
 interface SectionProps {
   title: string;
 }
 
-
 const Section: FC<SectionProps> = (props): JSX.Element => {
-  
-  const [showInput,SetShowInput]=useState(false)
-  const [inputValue,setInputValue]=useState("")
+  const [showInput, SetShowInput] = useState(false);
+  const [inputValue, setInputValue] = useState("");
 
-  const {state,dispatch}=useContext(AppContext)
+  const { state, dispatch } = useContext(AppContext);
+  const { customers, selectedCustomer } = state;
 
+  const { title } = props;
+  console.log(state, title);
 
-  const {title}=props
-  console.log(state,title)
+  const addOnClickhandler: Function = () => {
+    SetShowInput(true);
+  };
 
-  const addOnClickhandler:Function =()=>{
-    SetShowInput(true)
-  }
+  const onInputEnter: Function = (e: KeyboardEvent) => {
+    if (e.key === "Enter" && inputValue.length > 0) {
+      title === "Customers"
+        ? dispatch({
+            type: actionType.ADD_CUSTOMER,
+            payload: { name: inputValue },
+          })
+        : dispatch({
+            type: actionType.ADD_FEEDBACK,
+            payload: { feedback: inputValue },
+          });
 
-  const onInputEnter:Function=(e:KeyboardEvent)=>{
-   if(e.key=== "Enter"&&inputValue.length>0) {
-   title === "Customers" ? dispatch({type:actionType.ADD_CUSTOMER,payload:{name:inputValue}})
-   :dispatch({type:actionType.ADD_FEEDBACK,payload:{feedback:inputValue}})
-   SetShowInput(false)
-   }
-  }
+      SetShowInput(false);
+      setInputValue("");
+    }
+  };
 
+  const feedbacks = customers.find(
+    (customer) => customer.id === selectedCustomer
+  )?.feedbacks;
 
-  return <SectionWrapper>
-    <SectionHeader>
-    <SectionTitle>
-      {title}
-    </SectionTitle>
-    <AddButtton onClick={()=>addOnClickhandler()}> Add new </AddButtton>
-    </SectionHeader>
-    <SectionContent>
-      {showInput&&<StyledInput      
+  console.log("feedbacks", feedbacks);
+  const customerOnlickHandler: Function = (id: string) =>
+    dispatch({ type: actionType.SELECT_CUSTOMER, payload: { id } });
+
+  return (
+    <SectionWrapper>
+      <SectionHeader>
+        <SectionTitle>{title}</SectionTitle>
+        <AddButtton onClick={() => addOnClickhandler()}> Add new </AddButtton>
+      </SectionHeader>
+      <SectionContent>
+        {showInput && (
+          <StyledInput
             tabIndex={0}
-            onKeyDown={(e) => onInputEnter(e)} 
-            value={inputValue} 
-            onChange={e=>setInputValue(e.target.value)}/>}
-    </SectionContent>
-    </SectionWrapper>;
+            onKeyDown={(e) => onInputEnter(e)}
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+        )}
+        <StyledUl>
+          {title === "Customers" ? (
+            customers.length > 0 ? (
+              customers.map(({ name, id }) => (
+                <StyledListItem
+                  key={id}
+                  onClick={() => customerOnlickHandler(id)}
+                >
+                  <label>{name}</label>
+                </StyledListItem>
+              ))
+            ) : (
+              <NotifyLabel>{`No ${title}`}</NotifyLabel>
+            )
+          ) : feedbacks && feedbacks.length > 0 ? (
+            feedbacks.map((feedback) => (
+              <StyledListItem key={feedback}>
+                <label>{feedback}</label>
+              </StyledListItem>
+            ))
+          ) : (
+            <NotifyLabel>{`No ${title}`}</NotifyLabel>
+          )}
+        </StyledUl>
+      </SectionContent>
+    </SectionWrapper>
+  );
 };
 
 export default Section;
